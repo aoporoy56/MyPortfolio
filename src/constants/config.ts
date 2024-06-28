3ba46e8d53020748246d2ffa14ad2ceea523ff1c
aoporoy <aopo56@gmail.com>
2024-06-29 01:31:16 +0600

    initial commit

---
+++ src/constants/config.ts
@@ -0,0 +1,92 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Aopo's Portfolio",
+    fullName: 'Sri Aopo Chandra Das',
+    email: 'aopo56@gmail.com',
+  },
+  hero: {
+    name: 'Aopo Chandra Das',
+    p: ['I develop MERN Stack Web Application, user', 'interfaces and web applications'],
+  },
+  contact: {
+    p: 'Get in touch',
+    h2: 'Contact.',
+    form: {
+      name: {
+        span: 'Your Name',
+        placeholder: "What's your name?",
+      },
+      email: { span: 'Your Email', placeholder: "What's your email?" },
+      message: {
+        span: 'Your Message',
+        placeholder: 'What do you want to say?',
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: 'Introduction',
+      h2: 'Overview.',
+      content: `Hello! I'm Aopo, a passionate MERN stack developer skilled in MongoDB, Express.js, React, and Node.js. I create dynamic, responsive web applications that provide seamless user experiences. With a strong background in both frontend and backend development, I build robust and efficient solutions tailored to meet diverse needs. My portfolio showcases a range of projects, from e-commerce platforms to social media apps, reflecting my ability to tackle complex problems and deliver high-quality results. I'm always excited about new opportunities to collaborate and innovate. Let's connect and create something amazing together!
+
+`,
+    },
+    experience: {
+      p: 'What I have done so far',
+      h2: 'Work Experience.',
+    },
+    feedbacks: {
+      p: 'What others say',
+      h2: 'Testimonials.',
+    },
+    works: {
+      p: 'My work',
+      h2: 'Projects.',
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
